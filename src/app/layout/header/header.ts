8787530097889ea1f52f8d8c608c0f9d1418bf6a
CANOST-id <csinner31@gmail.com>
2026-06-18 21:51:41 +0200

    changed header button

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header {
+
+  btnClassList = "btn btn--primary";
+  path = "";
+  btn_text= "Neues Produkt";
+
+  ngOnInit() {
+    // if this path is on detail page, then the button changes
+    this.path = ""; 
+
+    if(this.path == "detail") {
+      this.btn_text = "Zurück zur Übersicht";
+    }
+  }
+}
