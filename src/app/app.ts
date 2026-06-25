e65e4d65f5e917fcdd4fea4e4c930773f4dd97ff
CANOST-id <csinner31@gmail.com>
2026-06-25 19:44:05 +0200

    changed component import into routing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
-import { ProductList } from './shared/components/product-list/product-list';
-import { ProductDetail } from './shared/components/product-detail/product-detail';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    Header,
-    ProductList,
-    ProductDetail],
+    Header],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
