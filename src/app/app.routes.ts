ad267f21e5e2e42a90966a22cc108f5375c4d7a1
CANOST-id <csinner31@gmail.com>
2026-06-29 21:39:17 +0200

    added error 404 Page - if path ist false

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductDetail } from './shared/components/product-detail/product-detail';
 import { ProductList } from './shared/components/product-list/product-list';
+import { Error404 } from './shared/components/error-404/error-404';
 
 
 export const routes: Routes = [
     {path: "", component: ProductList},
-    {path: "detail", component: ProductDetail}
+    {path: "detail/:name", component: ProductDetail},
+    {path: "**", component: Error404}
 ];
